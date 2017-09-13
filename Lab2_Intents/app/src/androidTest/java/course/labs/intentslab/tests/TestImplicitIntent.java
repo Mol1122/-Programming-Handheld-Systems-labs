a1ffa3d3508f1e071c083a36b074d6a284963a7a
Andrej Rasevic <andrej.petar.rasevic@gmail.com>
2017-09-13 10:53:43 -0400

    Added Lab2_Intents

---
+++ Lab2_Intents/app/src/androidTest/java/course/labs/intentslab/tests/TestImplicitIntent.java
@@ -0,0 +1,70 @@
+package course.labs.intentslab.tests;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.SdkSuppress;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject2;
+import android.support.test.uiautomator.Until;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+@RunWith(AndroidJUnit4.class)
+@SdkSuppress(minSdkVersion = 21)
+public class TestImplicitIntent {
+
+    private static final String BASIC_SAMPLE_PACKAGE
+            = "course.labs.intentslab";
+    private static final int LAUNCH_TIMEOUT = 5000;
+    private UiDevice mDevice;
+
+    @Before
+    public void startMainActivityFromHomeScreen() {
+        // Initialize UiDevice instance
+        mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+
+        // Start from the home screen
+        mDevice.pressHome();
+
+        // Wait for launcher
+        final String launcherPackage = mDevice.getLauncherPackageName();
+        assertThat(launcherPackage, is(notNullValue()));
+        mDevice.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)),
+                LAUNCH_TIMEOUT);
+
+        // Launch the app
+        Context context = InstrumentationRegistry.getContext();
+        final Intent intent = context.getPackageManager()
+                .getLaunchIntentForPackage(BASIC_SAMPLE_PACKAGE);
+
+        if (null == intent) fail();
+
+        // Clear out any previous instances
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        context.startActivity(intent);
+
+        // Wait for the app to appear
+        mDevice.wait(Until.hasObject(By.pkg(BASIC_SAMPLE_PACKAGE).depth(0)),
+                LAUNCH_TIMEOUT);
+    }
+
+    @Test
+    public void TestMyBrowserAvailable() {
+        mDevice.findObject(By.res(BASIC_SAMPLE_PACKAGE, "implicit_activation_button")).click();
+        UiObject2 chooserContents = mDevice.wait(Until.findObject(By.text("MyBrowser")), 2000);
+        assertNotNull(chooserContents);
+
+        mDevice.pressBack();
+    }
+}
